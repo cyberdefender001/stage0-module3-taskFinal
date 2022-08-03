a82baff7969aae2f7f7a15196c22b999e3c6ec25
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-03 16:53:51 +0500

    Update TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
-    public void toFahrenheit(int temperatureCelsius){
+    public static void main(String[] args){
+        toFahrenheit(26);
+    }
+    public static void toFahrenheit(int temperatureCelsius){
+        double cel = temperatureCelsius * 1.8 + 32;
+        System.out.println(cel);
     }
 }
