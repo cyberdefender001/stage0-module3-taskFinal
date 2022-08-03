9410146d9b70d713389f4a00bc782d316cf1321f
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-03 16:44:42 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public static void main(String[] args) {
+        calculateSum(1234);
+    }
+    public static void calculateSum(int number) {
+        int num1 = number / 1000;
+        int num2 = number % 1000 / 100;
+        int num3 = number % 100 / 10;
+        int num4 = number % 10;
+        int result = num4 + num3 + num2 + num1;
+        System.out.println(result);   
     }
 }
