330430f18d9cf175df901877e4623e8f37402431
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-03 10:03:44 +0500

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -1,7 +1,14 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
-    public void printIsPositive(int number) {
-
+    public static void printIsPositive(int number) {
+        if (number > 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
+    public static void main(String[] args) {
+        printIsPositive(14);
+    } 
 }
