6dc36dee4297520ab98c803784149ebe9a68bfc8
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-03 16:39:04 +0500

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+    public static void main(String[] args) {
+        revert(436);
     }
+    public static void revert(int number) {
+        int num1 = number / 100;
+        int num2 = number % 100 / 10;
+        int num3 = number % 10;
+        int result = num3 * 100 + num2 * 10 + num1;
+        System.out.println(result);
+    }
+     
 }
